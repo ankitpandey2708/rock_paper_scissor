b88da3872d84b8808b73bc3e98ba627fc00af310
AKP <ankitpandey2708@users.noreply.github.com>
2024-03-09 04:27:18 +0530

    x

--- rps1.js
+++ rps1.js
@@ -3,48 +3,48 @@ function compare(x, y) {
         return "draw";
     } else if ((x === 'rock' && y === 'scissor') || (x === 'paper' && y === 'rock') || (x === 'scissor' && y === 'paper')) {
         return "user wins";
-    } else
+    } else {
         return "computer wins";
+    }
 }
-
 
 function playGame() {
     let userScore = 0;
     let computerScore = 0;
+    let gameResult = "";
 
     for (let i = 0; i < 5; i++) {
-
         let userInput;
         while (!['rock', 'paper', 'scissor'].includes(userInput)) {
-            userInput = prompt("Please enter rock, paper, or scissor :").toLowerCase();
+            userInput = prompt("Please enter rock, paper, or scissor:").toLowerCase();
         }
 
         let computerInput = ['rock', 'paper', 'scissor'][Math.floor(Math.random() * 3)];
-        console.log("Random computer input:", computerInput);
-
+        // console.log("Random computer input:", computerInput);
         let roundResult = compare(userInput, computerInput);
-        console.log("Result of round", i + 1, ":", roundResult);
+        // console.log("Result of round", i + 1, ":", roundResult);
+
+        gameResult += "Round " + (i + 1) + ":\nUser's choice: " + userInput + "\nComputer's choice: " + computerInput + "\nResult: " + roundResult + "\n\n";
 
         if (roundResult === "user wins") {
             userScore++;
         } else if (roundResult === "computer wins") {
             computerScore++;
         }
-
     }
 
-    console.log("Final Scores:");
-    console.log("User:", userScore);
-    console.log("Computer:", computerScore);
+    // console.log("Final Scores:\nUser:", userScore, "\nComputer:", computerScore);
+    gameResult += "Final Scores:\nUser: " + userScore + "\nComputer: " + computerScore + "\n\n";
 
     if (userScore > computerScore) {
-        console.log("User wins the game!");
+        gameResult += "User wins the game!";
     } else if (userScore < computerScore) {
-        console.log("Computer wins the game!");
+        gameResult += "Computer wins the game!";
     } else {
-        console.log("It's a tie!");
+        gameResult += "It's a tie!";
     }
 
+    alert(gameResult);
 }
 
 playGame();
